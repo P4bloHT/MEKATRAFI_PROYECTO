0999a4052408f043e81a7ec9c67cd9ef34f59214
Carlos Vasquez Bernal <4z20carlos@gmail.com>
2024-06-25 22:53:38 -0500

    ACTULIZACION 2

--- MecaTrafiSystemGUIApp/usuario.h
+++ MecaTrafiSystemGUIApp/usuario.h
@@ -237,7 +237,11 @@ namespace MecaTrafiSystemGUIApp {
 			// 
 			// dataGridView1
 			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
 			this->dataGridView1->AllowUserToOrderColumns = true;
+			this->dataGridView1->AllowUserToResizeColumns = false;
+			this->dataGridView1->AllowUserToResizeRows = false;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(4) {
 				this->Codigo,
